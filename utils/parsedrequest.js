30b48a391b54f9ec2690499a126dfd0eb853103a
surelis segarra <sdarianne34@gmail.com>
2020-03-16 14:56:55 -0700

    createResponse passed

---
+++ utils/parsedrequest.js
@@ -0,0 +1,16 @@
+module.exports = rawRequest => {
+    //variable that raw request and you are going to split on to new nlines
+  
+    // another variable thats an array of method (get, post, put) and then split that based on the variable above so youre gonna use theat varuable from line 2 on line 4 and youre gonna split it HTTP/1.1...etc etc 
+  
+    //set this to  body that then splits on line breaks via carriage returns \r\n 
+  
+    //return 
+  
+  //   return {
+  //     variable1,
+  //     variable2,
+  //     varuable3
+  //   }
+  }
+  
